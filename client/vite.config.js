b3f421911c9d0a5d2ceca6c3d8700058596b2bfe
Mukundhan-K <mukundmugil@gmail.com>
2025-12-20 11:58:57 +0530

    fix: try to sortour _Redirects

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { resolve } from 'path';
 import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
@@ -29,6 +30,9 @@ export default defineConfig({
     sourcemap: false,
     minify: "esbuild",
     rollupOptions: {
+       input: {
+        main: resolve(__dirname, 'index.html'),
+      },
       output: {
         manualChunks: {
           react: ["react", "react-dom"],
@@ -38,6 +42,8 @@ export default defineConfig({
     },
   },
 
+  publicDir: 'public', // ensures public folder is copied to dist
+  
   server: {
     historyApiFallback: true
   }
