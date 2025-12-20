e12f8269bc67e93729a1920047a9fa5ffa08784b
Mukundhan-K <mukundmugil@gmail.com>
2025-12-20 12:30:05 +0530

    feat : unins package and changed vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+// import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { resolve } from 'path';
-import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '' // copy to dist root
-        }
-      ]
-    })
-    // compression({ algorithm: "brotliCompress", ext: ".br"}),
-    // compression({ algorithm: "gzip", ext: ".gz"}),
+    // viteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: 'public/_redirects',
+    //       dest: '' // copy to dist root
+    //     }
+    //   ]
+    // })
   ],
 
   base: '/',
@@ -28,22 +25,9 @@ export default defineConfig({
     outDir: 'dist',
     cssCodeSplit: true,
     sourcemap: false,
-    minify: "esbuild",
-    rollupOptions: {
-       input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          redux: ["react-redux"],
-        },
-      },
-    },
+    minify: "esbuild"
   },
 
-  publicDir: 'public', // ensures public folder is copied to dist
-  
   server: {
     historyApiFallback: true
   }
