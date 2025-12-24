0bd83043fdc736cef5d76bce14ac0f6bb5745cbe
Mukundhan-K <mukundmugil@gmail.com>
2025-12-24 15:30:25 +0530

    fix : deleted contact.js

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,10 @@ const connectCloudinary = require(path.join(__dirname,"config","cloudinary"));
 
 dbconnection();
 connectCloudinary();
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 // connection ---------------------------------------------
 
-app.listen(port, ()=>{
-    console.log("server run @ port : ", port);
-});
+// app.listen(port, ()=>{
+//     console.log("server run @ port : ", port);
+// });
